56b74d3a7899a6973665db70c09926bdb4c44142
hafijul233 <hafijul233@users.noreply.github.com>
2024-05-05 10:16:15 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Laraflow\Local\Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Laraflow\Local\LocalServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
